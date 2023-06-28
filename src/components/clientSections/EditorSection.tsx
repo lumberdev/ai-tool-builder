94e3a001f002827417cf32c2e83005d13bc28577
Maia Sason (work) <maia@lumber.dev>
2023-06-28 01:01:46 -0400

    docs: added comment

--- src/components/clientSections/EditorSection.tsx
+++ src/components/clientSections/EditorSection.tsx
@@ -2,6 +2,7 @@
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 const EditorComponent = dynamic(
+  // Import dynamically because it doesn't support SSR
   () => import("~/components/editor/Editor").then((res) => res.EditorComponent),
   {
     ssr: false,
